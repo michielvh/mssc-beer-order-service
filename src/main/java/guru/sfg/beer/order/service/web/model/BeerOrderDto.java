662ad7f56c4dae60c2128463b5d90d65a3cb907d
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-09-09 18:51:25 +0200

    edit web model for beerOrderStatusEnum  #17

--- src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderDto.java
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderDto.java
@@ -17,14 +17,13 @@
 
 package guru.sfg.beer.order.service.web.model;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
-import java.time.OffsetDateTime;
-import java.util.List;
-import java.util.UUID;
 
 @Data
 @NoArgsConstructor
@@ -33,7 +32,7 @@ public class BeerOrderDto extends BaseItem {
 
     @Builder
     public BeerOrderDto(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate, UUID customerId, List<BeerOrderLineDto> beerOrderLines,
-                        BeerOrderStatusEnum orderStatus, String orderStatusCallbackUrl, String customerRef) {
+                        String orderStatus, String orderStatusCallbackUrl, String customerRef) {
         super(id, version, createdDate, lastModifiedDate);
         this.customerId = customerId;
         this.beerOrderLines = beerOrderLines;
@@ -45,6 +44,6 @@ public class BeerOrderDto extends BaseItem {
     private UUID customerId;
     private String customerRef;
     private List<BeerOrderLineDto> beerOrderLines;
-    private BeerOrderStatusEnum    orderStatus;
+    private String    orderStatus;
     private String                 orderStatusCallbackUrl;
 }
